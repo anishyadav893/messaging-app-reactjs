0492d9daea83a35b840f887b4714e94a7c85531a
Anish Yadav <yadavanish893@gmail.com>
2021-07-06 21:46:19 -0700

    final push

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import { ChatEngine } from 'react-chat-engine';
 
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
+const projectID = '48055bcd-50f7-4fbf-8c93-a1f602a2328f';
+
 const App = () => {
-	return (
-		<ChatEngine
-		height = '100vh'
-		projectID = ''
-		userName = ''
-		userSecret = ''
-		 />
-	);
-}
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
 
 export default App;
